fb24e8c31f615c43c4031fc31bf2edebf7bd31f6
trashiodev <firemonstr2002@mail.ru>
2021-12-18 00:15:35 +0300

    Init

---
+++ Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetworkContract
+{
+    [Serializable]
+    public class Info
+    {
+        public InfoType type;
+        public byte[] bytes;
+    }
+}
+
---
+++ InfoType.cs
@@ -0,0 +1,15 @@
+﻿namespace NetworkContract
+{
+    public enum InfoType
+    {
+        // Connection
+        Disconnect = 1,
+
+        // Chat
+        ChatMessage = 1000,
+
+        // Gameplay
+        NewPlayerPlayerConnected = 2000,
+        MovePlayer = 2100,
+    }
+}
