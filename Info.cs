c5a987bf0810bc36c19da8445133d8ed184c384d
trashiodev <firemonstr2002@mail.ru>
2021-12-18 00:34:51 +0300

    info && infotype correctings

--- Info.cs
+++ Info.cs
@@ -5,6 +5,7 @@ namespace NetworkContract
     [Serializable]
     public class Info
     {
+        public short sender;
         public InfoType type;
         public byte[] bytes;
     }
--- InfoType.cs
+++ InfoType.cs
@@ -4,12 +4,12 @@
     {
         // Connection
         Disconnect = 1,
-
+        NewPlayerConnected = 2,
+        
         // Chat
         ChatMessage = 1000,
 
         // Gameplay
-        NewPlayerPlayerConnected = 2000,
-        MovePlayer = 2100,
+        MovePlayer = 2000,
     }
 }
